e40c8fe9f6cda0fb64190cf49b863f5e32b067c8
Kukaracka <kukaracka@inbox.ru>
2025-10-11 12:55:47 +0700

    update and delete endpoints has been created

--- backend/src/api/users_endpoints.py
+++ backend/src/api/users_endpoints.py
@@ -13,6 +13,7 @@ user_router = APIRouter(
 async def create_user(user): # TODO: pydantic schema
     return {"user_id": "Moc responce"}
 
+
 @user_router.get("/",
                  response_description="List of users retrieved seccessfully",
                  tags=["users"])
@@ -20,3 +21,18 @@ async def read_users(): # TODO: add pydantic schema
     user_data = "Moc responce"
     return user_data
 
+
+@user_router.delete("/",
+                    response_description="User has been deleted")
+async def detele_user(user_id):
+    # TODO: delete logic  
+    return {"user_id": user_id,
+            "deleted": True}
+
+
+@user_router.patch("/",
+                   response_description="User has been updated")
+async def update_user(user_id, data):
+    # TODO: update logic
+    return {"user_id": user_id,
+            "updated": True}
