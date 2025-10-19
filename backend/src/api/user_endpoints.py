14c94d4ea3729790bcec8ade1327290e72ac10c7
Kukaracka <kukaracka@inbox.ru>
2025-10-19 21:13:20 +0700

    an endpoint for getting a list of users was implemented

---
+++ backend/src/api/user_endpoints.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter
+
+from repositories.user_repository import UserRepository
+from schemas.user_schemas import UserReadSchema
+from services.user_service import UserService
+
+user_router = APIRouter(prefix="/users", tags=["Users"])
+
+
+# TODO: pydantic schema
+@user_router.post(
+    "/",
+    response_description="New user has been created",
+)
+async def create_user(user):  # TODO: pydantic schema
+    return {"user_id": "Moc responce"}
+
+
+@user_router.get(
+    "/",
+    response_description="List of users retrieved successfully",
+    response_model=list[UserReadSchema],
+)
+async def get_users() -> list[UserReadSchema]:
+    """
+    This endpoint is used to get a list of users
+    """
+    user_data = await UserService(UserRepository).read_all_users()
+    return user_data
+
+
+@user_router.delete("/", response_description="User has been deleted")
+async def detele_user(user_id):
+    # TODO: delete logic
+    return {"user_id": user_id, "deleted": True}
+
+
+@user_router.patch("/", response_description="User has been updated")
+async def update_user(user_id, data):
+    # TODO: update logic
+    return {"user_id": user_id, "updated": True}
