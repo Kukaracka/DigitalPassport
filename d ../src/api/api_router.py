0b4e615f204a75657935e18e7053f6c3ed79a80d
Kukaracka <kukaracka@inbox.ru>
2025-10-24 23:37:36 +0700

    remove dublicates

--- d ../src/api/api_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from api import auth_endpoints, user_endpoints, debug_endpoints
-
-api_router = APIRouter()
-api_router.include_router(user_endpoints.user_router)
-api_router.include_router(auth_endpoints.auth_router)
-api_router.include_router(debug_endpoints.debug_router)
